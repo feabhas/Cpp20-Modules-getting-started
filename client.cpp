d7efa6c72cd1ef0c7400e7aee8db996379afc273
Niall Cooling <niall.cooling@gmail.com>
2021-07-28 16:16:42 +0100

    global fragment

--- client.cpp
+++ client.cpp
@@ -1,9 +1,20 @@
-// client.cpp
+// client1.cpp
+
 #include <iostream>
+#include <vector>
 
 import my_math;
 
 int main() {
-   auto rv = add(2000, 20);
-   std::cout << rv << '\n';  
+    
+    std::cout << std::endl;   
+   
+    std::cout << "add(2000, 20): " << add(2000, 20) << std::endl;
+    
+    std::vector<int> myVec{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    
+    std::cout << "getProduct(myVec): " << getProduct(myVec) << std::endl;
+    
+    std::cout << std::endl;
+   
 }
--- my_math.cxx
+++ my_math.cxx
@@ -1,7 +1,15 @@
-// my_math.cxx
+// my_math1.cxx
+module;                   // global module fragment (1)
 
-export module my_math;
+#include <numeric>
+#include <vector>
+
+export module my_math;       // module declaration (2)
 
 export int add(int fir, int sec){
     return fir + sec;
-} 
+}
+
+export int getProduct(const std::vector<int>& vec) {
+    return std::accumulate(vec.begin(), vec.end(), 1, std::multiplies<int>());
+}
